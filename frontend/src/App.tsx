b9ec449aa72c545b14a5b3992c1e04fd8f576551
MREZA <rezaworku31@gmail.com>
2026-04-23 00:05:16 +0700

    chore : run build and fix(backend url)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://192.168.100.19:3000'
 
 export default function App() {
   const [file, setFile] = useState<File | null>(null)
