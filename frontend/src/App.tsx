0d5465ca4fdbd10998bc0b435b4bf230cbaa7de8
MREZA <rezaworku31@gmail.com>
2026-04-22 23:55:35 +0700

    chore : fe code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,121 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const BASE_URL = 'http://localhost:3000'
+
+export default function App() {
+  const [file, setFile] = useState<File | null>(null)
+  const [code, setCode] = useState('')
+  const [uploadedCode, setUploadedCode] = useState('')
+  const [loading, setLoading] = useState(false);
+
+  const handleUpload = async () => {
+    if (!file) return alert("Pilh file terlebih dahulu");
+
+    const formData = new FormData();
+    formData.append('file', file)
+
+    setLoading(true)
+
+    try {
+      const res = await fetch(`${BASE_URL}/upload`, {
+        method: "POST",
+        body: formData,
+      })
+
+      const data = await res.json()
+      setUploadedCode(data.code)
+
+    } catch (error) {
+      console.log(error);
+      alert('Upload Gagal')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleDownload = () => {
+    if (!code) return alert("Masukkan kode");
+    window.open(`${BASE_URL}/download/${code}`, '_blank')
+
+  }
+
+
+  const handlePreview = () => {
+    if (!code) return alert("Masukkan kode");
+    window.open(`${BASE_URL}/file/${code}`)
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Simple file share</h1>
+      <div style={{ marginBottom: 30 }}>
+        <h2>Upload</h2>
+        <input type="file" onChange={(e) => setFile(e.target.files?.[0] || null)}></input>
+        <br />
+        <br />
+        <button onClick={handleUpload} disabled={loading}>{loading ? "Uploading ..." : 'Upload'}</button>
+        {
+          uploadedCode && (
+            <p>
+              Kode : <b>{uploadedCode}</b>
+            </p>
+          )
+        }
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          <p>Code</p>
+          <h2>Download / Preview</h2>
+          <input value={code} onChange={(e) => setCode(e.target.value)} placeholder="Masukkan code"></input>
+          <br />
+          <br />
+          <button onClick={handleDownload}>Download</button>
+          <button onClick={handlePreview} style={{ marginLeft: 10 }}>Preview</button>
         </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      </div>
+    </div>
   )
 }
-
-export default App
